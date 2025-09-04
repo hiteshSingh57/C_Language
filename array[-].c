d453b9c15eef9540a82f698f732a3f15abcb0d65
hiteshSingh57 <hiteshsingh001361@gmail.com>
2025-09-04 20:12:08 +0530

    update file

--- array[-].c
+++ array[-].c
@@ -1,18 +1,21 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
+
+int main()
 {
     int array[5],i;
+
     printf("Enter element in array:\n");
     for(i = 0; i < 5;i++)
     {
         scanf("%d",&array[i]);
     }
+    
     printf("Array Elements:");
     
     for ( i = 0; i < 5; i++)
     {
         printf(" %d",array[i]);
     }
-    
+
+    return 0;
 }
